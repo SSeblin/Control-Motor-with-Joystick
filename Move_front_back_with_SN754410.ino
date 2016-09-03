e2c85d178211ed9cbbd4066f7d15b97d21c10737
SSeblin <sseblin@appstud.me>
2016-09-03 16:45:18 +0200

    Update Move_front_back_with_SN754410.ino

--- Move_front_back_with_SN754410.ino
+++ Move_front_back_with_SN754410.ino
@@ -1,34 +1,38 @@
 /*
 FOR THIS MODULE, I AM TRYING TO MAKE A MOTOR MOVE FRONT AND 
-BACK WITH A H BRIDGE SN754410
+BACKWARD WITH A H BRIDGE SN754410 THE LEFT AND RIGHT IS MANAGED 
+WITH A SERVO MOTOR. 
 */
+
+#include <Servo.h>
+Servo servoWheel;
+
 //BEGIN H-BRIDGE DECLARATION
 const int controlPin1=2;
 const int controlPin2=3;
 const int enablePin=9;
 
+int angle; //Angle of the Servo
 
-// BEGIN JOYSTICK DECLARATIONS
-//Define (analog) pins for Joystick
+//Define analog pins for Joystick
 const int VRx =0;
 const int VRy =1;
-const int SW =2;
-// Read variables
-int calX, calY, calSW, rawX, rawY, rawSW;
+
+
+int calY, rawY;
 
 
 void setup() {
 
+  servoWheel.attach(10); //The digital pin where the servo is attached
   Serial.begin(9600); //Initialize serie port
 
 pinMode (controlPin1, OUTPUT);
 pinMode (controlPin2, OUTPUT);
 pinMode (enablePin, OUTPUT);
 
-//JOYSTICK CALIBRATION TO (0,0)
-calX = analogRead(VRx);
+//JOYSTICK CALIBRATION ON Y
 calY = analogRead(VRy);
-calSW = analogRead(SW);
   //END JOYSTICK CALIBRATION
 
 digitalWrite (enablePin,LOW);
@@ -38,37 +42,42 @@ digitalWrite (enablePin,LOW);
 void loop() {
 
 
-//HERE IS TO MEASURE VALUES OF (X, Y) SENT BY JOYSTICK
-rawX = analogRead(VRx)- calX;
+//HERE IS TO MEASURE VALUES OF X AND Y SENT BY JOYSTICK
+rawX = analogRead(VRx);
 rawY = analogRead(VRy)- calY;
-rawSW = analogRead(SW)- calSW;
 
 // AND TO DISPLAY THEM ON MY SCREEN
 Serial.print("Valeur X : ");
 Serial.println((int)(rawX));
 Serial.print("Valeur y : ");
 Serial.println((int)(rawY));
-Serial.print("Switch : ");
 //END OF DISPLAY MODULE 
 
-
-if (rawY>10){ //If joystick is used following the growing Y axis
+// MOVE FORWARD MODULE 
+if (rawY>100){ //If joystick is used following the growing Y axis
   digitalWrite(enablePin, HIGH);
       digitalWrite(controlPin1, HIGH);
       digitalWrite(controlPin2,LOW);
       } 
       
-else if (rawY<-10){ //If joystick is used following the decreasing Y axis
+else if (rawY<-100){ //If joystick is used following the decreasing Y axis
       digitalWrite(enablePin, HIGH);
       digitalWrite(controlPin1, LOW);
       digitalWrite(controlPin2,HIGH);
       }
-// Waiting 60 ms won't hurt any one
-//delay(60);
 
  else {
       digitalWrite(enablePin, LOW);
 }
 
+
+//MOVE LEFT OR RIGHT MODULE 
+
+  angle = map(rawX,0,1023,0,180);
+  Serial.print(", Angle: ");
+  Serial.println(angle);
+
+  servoWheel.write(angle);
+  delay(30);
 
 } //THIS IS THE END
